69b056406ad4f9ea2cfa4c60613454b15f7f78b9
Ben <bendeverett@gmail.com>
2014-04-08 17:20:47 -0400

    important setup fix, including numpy path

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #from distutils.core import setup, Extension #it's either this line (distutils) or the next (setuptools). both work, using setuptools because it supports develop mode
 from setuptools import find_packages, setup, Extension
-import numpy
 from distutils.sysconfig import get_python_inc
 readme = open('README.txt').read()
 import os
+import numpy as np
 
 incdir = os.path.join(get_python_inc(plat_specific=1), 'Numerical')
 
@@ -21,6 +21,6 @@ setup(	name = 'PyFluo',
 												],
 		install_requires = ['numpy', 'scipy', 'matplotlib'], 
 		ext_modules=[ Extension('_tifffile', ['pyfluo/tiff/_installation/tifffile.c'],include_dirs=[incdir]) ],
-        include_dirs = [incdir]
+        include_dirs = [incdir, np.get_include()]
         
         )
